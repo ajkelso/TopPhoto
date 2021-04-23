a3188e8a6364b09499444091859a383586e6365e
ajkelso <ajkelso@gmail.com>
2021-04-23 11:08:03 -0400

    sadd packages to frontend

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Login from './Login'
+import SignUp from './SignUp'
+
+function Home(props) {
+
+    const [login, setLogin] = useState(false)
+    const [signUp, setSignUp] = useState(false)
+
+    const handleLoginClick = () => {
+        setLogin(true)
+        if (signUp) {
+            setSignUp(false)
+        }
+    }
+
+    const handleSignupClick = () => {
+        setSignUp(true)
+        if (login) {
+            setLogin(false)
+        }
+    }
+
+    return (
+        
+        <div>
+            
+            <h2 class="d-flex justify-content-center">Welcome!</h2>  
+            <div class="d-flex justify-content-center">
+                <Button variant="outline-info" onClick={handleLoginClick}>Login</Button>{' '}
+                <Button variant="outline-info" onClick={handleSignupClick}>SignUp</Button>  
+            </div>
+            { login ? <Login history={props.history} /> : null }
+            { signUp ? <SignUp history={props.history}/> : null }
+
+        </div>
+    )
+}
+
+export default Home
