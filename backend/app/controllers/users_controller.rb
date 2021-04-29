05f815188f239b3ea447ef07e2210c2f4a7a31b4
ajkelso <ajkelso@gmail.com>
2021-04-28 20:31:58 -0400

    create user scontroller & create action

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token, message: "Welcome, #{@user.username}!" }, status: :created
+        else
+            render json: { error: @user.errors.full_messages.to_sentence }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+
+end
