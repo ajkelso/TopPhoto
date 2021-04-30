3ed64562fd512f4db4cf9e2e025cb2bc159ea980
ajkelso <ajkelso@gmail.com>
2021-04-30 15:01:48 -0400

    add ADD_GALLERY to userReducer

--- frontend/src/redux/userReducer.js
+++ frontend/src/redux/userReducer.js
@@ -2,7 +2,13 @@ export function userReducer(state = {}, action) {
     switch (action.type) {
         case 'SET_USER':
             return action.payload
+        case 'ADD_GALLERY':
+            return {
+                ...state,
+                galleries: [...state.galleries, action.payload],
+            }
         default:
             return state
+
     }
 }
