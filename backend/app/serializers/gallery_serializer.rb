1c785ba38d71e7db1d6f5c63095fa2ecd2697411
ajkelso <ajkelso@gmail.com>
2021-04-30 14:04:44 -0400

    adjust serialization of images

--- backend/app/serializers/gallery_serializer.rb
+++ backend/app/serializers/gallery_serializer.rb
@@ -1,3 +1,14 @@
 class GallerySerializer < ActiveModel::Serializer
-  attributes :id, :title, :goal, :photos
+  attributes :id, :title, :goal, :images
+
+  def images
+    object.photos.map do |photo|
+      {
+        id: photo.id,
+        favorites: photo.favorites,
+        url: photo.image_url
+      }
+    end
+  end
+
 end
