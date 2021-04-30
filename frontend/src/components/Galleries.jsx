dfcd07987ded9a58202baa7059546f6c411c1bee
ajkelso <ajkelso@gmail.com>
2021-04-30 15:12:50 -0400

    test & pass render galleries

--- frontend/src/components/Galleries.jsx
+++ frontend/src/components/Galleries.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Galleries(){
 
+    const galleries = useSelector(state => state.user.galleries)
 
+    const renderGalleries = () => {
+        return galleries.map(gal => <h4>{gal.title}</h4>)
+    }
 
     return(
         <div>
             Galleries Comp
+            {renderGalleries()}
         </div>
     )
 }
