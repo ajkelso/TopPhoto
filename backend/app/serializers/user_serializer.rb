ff94515cfbb5417f5c98c2f649506f630894a51c
ajkelso <ajkelso@gmail.com>
2021-04-30 14:57:43 -0400

    add galleries to User Serializer

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email 
+  attributes :id, :username, :email, :galleries 
+
+  def galleries
+    object.galleries.map do |gal|
+      GallerySerializer.new(gal)
+    end
+  end
 end
