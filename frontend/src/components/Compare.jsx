8279d1bc780c0926754f3426b09f2b41d5e241cd
ajkelso <ajkelso@gmail.com>
2021-05-03 14:19:05 -0400

    render compare images

--- frontend/src/components/Compare.jsx
+++ frontend/src/components/Compare.jsx
@@ -3,24 +3,38 @@ import { useDispatch } from 'react-redux'
 import { getGalleryPhotos } from '../redux/actions/galleryActions'
 
 function Compare(props) {
-
-    //get all photos in gallery
     //render first 2 side-by-side
     //allow user to chose the best one (or both or niether)
     //replace the 2 images with the next 2 in array
     //finish and start over?
+    
     const [currGallery, setCurrGallery] = useState({})
     const dispatch = useDispatch()
 
-    console.log(props.match.params.id)
+    let galleryPosition = 0
+
+    console.log(galleryPosition)
 
     useEffect(() => {
         dispatch(getGalleryPhotos(props.match.params.id, setCurrGallery))
     }, [])
+
+    const renderComparison = () => {
+        return (
+            <div>
+                <img className="photo-contest" src={currGallery.photos[galleryPosition].url} alt="photo 1"/>
+                <img className="photo-contest" src={currGallery.photos[galleryPosition + 1].url} alt="photo 2"/>
+            </div>
+        )
+    }
+    // console.log(currGallery.photos[galleryPosition].url)
     
     return(
         <div>
-            Compare
+            <h2>Compare</h2>
+            <h4>{currGallery.title}</h4>
+            { currGallery.goal ? <p>GOAL: {currGallery.goal}</p> : null }
+            { currGallery.photos ? renderComparison() : null }
         </div>
     )
 }
