0df922a832aa6b973d716061f12dfe46d98ec097
ajkelso <ajkelso@gmail.com>
2021-05-03 21:27:14 -0400

    adjust Compare to continue through rounds

--- backend/app/serializers/gallery_serializer.rb
+++ backend/app/serializers/gallery_serializer.rb
@@ -2,16 +2,15 @@ class GallerySerializer < ActiveModel::Serializer
   attributes :id, :title, :goal, :photos
 
   def photos
-    photos = object.photos.sort_by { |p| p.favorites }.reverse!.map do |photo|
+    photos = object.photos.sort_by { |p| p.favorites }.reverse!
+    top_photos = photos.select {|p| p.favorites == photos[0].favorites}   
+    top_photos.map do |photo|
       {
         id: photo.id,
         favorites: photo.favorites,
         url: photo.image_url
       }
     end
-
-
-
   end
 
 end
--- frontend/src/components/Compare.jsx
+++ frontend/src/components/Compare.jsx
@@ -5,7 +5,6 @@ import { upvoteRequest } from '../services/api'
 
 function Compare(props) {
     //allow user to chose the best one (or both or niether)
-    //replace the 2 images with the next 2 in array
     //finish and start over?
     
     const [currGallery, setCurrGallery] = useState({})
@@ -13,16 +12,12 @@ function Compare(props) {
     const [roundCount, setRoundCount] = useState(1)
 
     const dispatch = useDispatch()
-
-    let galleryPosition = 0
-
-    console.log(galPos)
-
+    
     useEffect(() => {
-        console.log("effectFire");
+        console.log("effectFire")
         dispatch(getGalleryPhotos(props.match.params.id, setCurrGallery))
     }, [roundCount])
-
+    
     const handlePhotoClick = (e) => {
         upvoteRequest(e.target.id)
         setGalPos(galPos + 2)
@@ -32,13 +27,19 @@ function Compare(props) {
         setRoundCount(roundCount + 1)
         setGalPos(0)
     }
-    const renderComparison = () => {
 
+    const renderComparison = () => {
         const imageA = () => {
             if(galPos < currGallery.photos.length){
                 return <img className="photo-contest" id={currGallery.photos[galPos].id} onClick={handlePhotoClick} src={currGallery.photos[galPos].url} alt="photo 1"/>
             } else {
-                return <p>All Images Have Been Tested</p>
+                return (
+                    <div>
+                        <p>Round Complete!</p>
+                        <button onClick={nextRound}>Click to start Round {roundCount + 1}</button>
+                        
+                    </div>
+                )
             }
         }
 
@@ -46,7 +47,7 @@ function Compare(props) {
             if(galPos + 1 < currGallery.photos.length){
                 return <img className="photo-contest" id={currGallery.photos[galPos + 1].id} onClick={handlePhotoClick} src={currGallery.photos[galPos + 1].url} alt="photo 2"/>
             } else if (galPos < currGallery.photos.length){
-                return <button onClick={nextRound}>Skip</button>
+                return <button onClick={setGalPos(galPos + 1)}>Skip</button>
             }
         }
 
@@ -57,14 +58,30 @@ function Compare(props) {
             </div>
         )
     }
+
+    const renderPhotos = () => {
+        if(currGallery.photos){
+            if (currGallery.photos.length === 1){
+                return(
+                    <div>
+                        <h3>Your favorite photo is:</h3>
+                        <img className="favorite-photo" id={currGallery.photos[0].id} onClick={handlePhotoClick} src={currGallery.photos[0].url} alt="Favorite Photo"/>
+                    </div>
+                )
+            } else {
+                return renderComparison()
+            }
+        }
+    }
     
     return(
         <div>
+            {console.log(currGallery)}
             <h2>Compare</h2>
             <h4>{currGallery.title}</h4>
             <h5>Round: {roundCount}</h5>
             { currGallery.goal ? <p>GOAL: {currGallery.goal}</p> : null }
-            { currGallery.photos ? renderComparison() : null }
+            { renderPhotos() }
         </div>
     )
 }
