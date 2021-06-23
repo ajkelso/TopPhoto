2dbda92f180bb96c63952eb3da7e2ba70701d0d9
ajkelso <ajkelso@gmail.com>
2021-06-23 11:34:46 -0400

    add gallery title if none exists in gallery controller

--- backend/app/controllers/galleries_controller.rb
+++ backend/app/controllers/galleries_controller.rb
@@ -7,7 +7,8 @@ class GalleriesController < ApplicationController
     
     def create
 
-        @gallery = Gallery.create(user_id: @user.id, title: gallery_params[:title])
+        @gallery = Gallery.create(user_id: @user.id, title: (gallery_params[:title] == "" ? "Gallery #{current_user.galleries.length + 1}" : gallery_params[:title]))
+        
         gallery_params[:images].map do |img|
             Photo.create(image: img, gallery_id: @gallery.id) # attaches the uploaded file
         end
