43182b424d08d6e6a4ee76ce8978a500766c00a3
ajkelso <ajkelso@gmail.com>
2021-07-12 13:05:58 -0400

    add hover to galleries

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -11,28 +11,28 @@ export default function Navigation() {
 
     const loggedInNav = () => {
         return(
-        <Navbar bg="light" variant="light" expand="sm" > 
-            <Navbar.Brand as={NavLink} to="/"><img className="logo" src="../TopPhotoLogo.png" atl="topPhoto-logo"/>  </Navbar.Brand>
             <Nav className="mr-auto">   
                 <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
                 <Nav.Link as={NavLink} to='/logout'>Logout</Nav.Link>
             </Nav>  
-        </Navbar>
         )
     }
 
-    const loggedOutNav = () => {
-        return (
-        <Navbar bg="light" variant="light" expand="sm" > 
-            <Navbar.Brand as={NavLink} to="/">TopPhoto</Navbar.Brand>
-        </Navbar>
-        )
-    }
+    // const loggedOutNav = () => {
+    //     return (
+    //     <Navbar bg="light" variant="light" expand="sm" > 
+    //         <Navbar.Brand as={NavLink} to="/">TopPhoto</Navbar.Brand>
+    //     </Navbar>
+    //     )
+    // }
     
     
     return(
         <div>
-            { user.id ? loggedInNav() : loggedOutNav() }
+            <Navbar bg="light" variant="light" expand="sm" > 
+                <Navbar.Brand as={NavLink} to="/"><img className="logo" src="../TopPhotoLogo.png" atl="topPhoto-logo"/>  </Navbar.Brand>
+                { user.id ? loggedInNav() : null }
+            </Navbar>
         </div>
     )
 }
